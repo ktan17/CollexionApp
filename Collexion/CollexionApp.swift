7ca1a8945120c18cd04e8fb1cef18c1407e2394f
Kevin Tan <ktan17@g.ucla.edu>
2021-12-27 11:30:02 -1000

    Initial Commit

---
+++ Collexion/CollexionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollexionApp.swift
+//  Collexion
+//
+//  Created by Kevin Tan on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct CollexionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
